daa0ce78dd1766489e9fc225b02edf02cb850781
Zed-chi <zed-chi@mail.ru>
2018-08-11 14:14:25 +0300

    edited funcs with min/max

--- bars.py
+++ bars.py
@@ -11,48 +11,34 @@ def load_data(filepath):
 
 
 def get_biggest_bar(json_data):
+    def get_seat(x):
+            return x["properties"]["Attributes"]["SeatsCount"]
     if json_data is not None:
-        biggest = [0, ""]
-        for bar in json_data["features"]:
-            bar_name = bar["properties"]["Attributes"]["Name"]
-            seats_num = bar["properties"]["Attributes"]["SeatsCount"]
-            if biggest[0] < seats_num:
-                biggest[0] = seats_num
-                biggest[1] = bar_name
-        return biggest
+        biggest_bar = max(json_data["features"], key=get_seat)
+        bar_name = biggest_bar["properties"]["Attributes"]["Name"]
+        seats = biggest_bar["properties"]["Attributes"]["SeatsCount"]
+        return [seats, bar_name]
 
 
 def get_smallest_bar(json_data):
+    def get_seat(x):
+        return x["properties"]["Attributes"]["SeatsCount"]
     if json_data is not None:
-        smallest = []
-        for bar in json_data["features"]:
-            bar_name = bar["properties"]["Attributes"]["Name"]
-            seats_num = bar["properties"]["Attributes"]["SeatsCount"]
-            if len(smallest) == 0:
-                smallest.append(seats_num)
-                smallest.append(bar_name)
-                continue
-            if smallest[0] > seats_num:
-                smallest[0] = seats_num
-                smallest[1] = bar_name
-        return smallest
+        smallest_bar = min(json_data["features"], key=get_seat)
+        bar_name = smallest_bar["properties"]["Attributes"]["Name"]
+        seats = smallest_bar["properties"]["Attributes"]["SeatsCount"]
+        return [seats, bar_name]
 
 
 def get_closest_bar(json_data, long, lat):
+    def get_length(bar):
+        coords = bar["geometry"]["coordinates"]
+        length = sqrt(abs(long-coords[0])**2 + abs(lat-coords[1])**2)
+        return length
     if json_data is not None and long is not None and lat is not None:
-        closest = []
-        for bar in json_data["features"]:
-            bar_name = bar["properties"]["Attributes"]["Name"]
-            coords = bar["geometry"]["coordinates"]
-            length = sqrt(abs(long-coords[0])**2 + abs(lat-coords[1])**2)
-            if len(closest) == 0:
-                closest.append(length)
-                closest.append(bar_name)
-                continue
-            if closest[0] > length:
-                closest[0] = length
-                closest[1] = bar_name
-        return closest
+        closest = min(json_data["features"], key=get_length)
+        bar_name = closest["properties"]["Attributes"]["Name"]
+        return bar_name
 
 
 if __name__ == '__main__':
@@ -62,7 +48,7 @@ if __name__ == '__main__':
     long = float(input("Введите долготу: ").strip())
     big_bar = get_biggest_bar(json_data)
     small_bar = get_smallest_bar(json_data)
-    closest_bar = get_closest_bar(json_data, long, lat)[1]
-    print("Самый большой бар '{x[1]}' - {x[0]} мест".format(big_bar))
-    print("Самый маленький бар '{x[1]}' - {x[0]} мест".format(small_bar))
-    print("Самый близкий бар {x[1]}".format(closest_bar))
+    closest_bar = get_closest_bar(json_data, long, lat)
+    print("Самый большой бар '{x[1]}' - {x[0]} мест".format(x=big_bar))
+    print("Самый маленький бар '{x[1]}' - {x[0]} мест".format(x=small_bar))
+    print("Самый близкий бар '{}'".format(closest_bar))
