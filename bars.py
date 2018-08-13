039b9924e349d855562950dc66ceefe0c406343a
AlexE <zed-chi@mail.ru>
2018-08-13 11:07:41 +0300

    some fixes
    - dublicate code in coords
    - arguments replaced to main()
    - fetching json from web removed

--- bars.py
+++ bars.py
@@ -1,6 +1,4 @@
 import json
-import sys
-import requests
 from math import sqrt
 
 
@@ -18,64 +16,54 @@ def get_coords():
     return lat, long
 
 
-def get_distance(long=None, lat=None, bar=None):
-    if lat is None and bar is None:
-        return lambda lat, bar: sqrt(
-            abs(long-bar["geometry"]["coordinates"][0])**2
-            + abs(lat-bar["geometry"]["coordinates"][1])**2)
-    elif bar is None:
+def get_distance(long, lat, bar=None):
+    if bar is None:
         return lambda bar: sqrt(
             abs(long-bar["geometry"]["coordinates"][0])**2
             + abs(lat-bar["geometry"]["coordinates"][1])**2)
 
 
-def load_data():
-    try:
-        path_to_json = sys.argv[1]
-        with open(path_to_json, "r", encoding="utf-8") as json_file:
-            return json.loads(json_file.read())["features"]
-    except IndexError:
-        url = ("https://devman.org/media/filer_public/"
-               "95/74/957441dc-78df-4c99-83b2-e93dfd13c2fa/bars.json")
-        res = requests.get(url)
-        return json.loads(res.text)["features"]
-    except ValueError:
-        raise ValueError("Invalid JSON")
+def load_data(path_to_json):
+    with open(path_to_json, "r", encoding="utf-8") as json_file:
+        return json.loads(json_file.read())["features"]
 
 
-def get_biggest_bar(json_content):
-    if json_content is not None:
-        biggest_bar = max(json_content, key=get_seats)
+def get_biggest_bar(decoded_json):
+    if decoded_json is not None:
+        biggest_bar = max(decoded_json, key=get_seats)
         return biggest_bar
 
 
-def get_smallest_bar(json_content):
-    if json_content is not None:
-        smallest_bar = min(json_content, key=get_seats)
+def get_smallest_bar(decoded_json):
+    if decoded_json is not None:
+        smallest_bar = min(decoded_json, key=get_seats)
         return smallest_bar
 
 
-def get_closest_bar(json_content, long, lat):
-    if json_content is not None and long is not None and lat is not None:
-        closest = min(json_content, key=get_distance(long, lat))
+def get_closest_bar(decoded_json, long, lat):
+    if decoded_json is not None and long is not None and lat is not None:
+        closest = min(decoded_json, key=get_distance(long, lat))
         return closest
 
 
 def main():
     try:
-        json_content = load_data()
-        big_bar = get_biggest_bar(json_content)
-        small_bar = get_smallest_bar(json_content)
+        path_to_json = input("Введите путь до json файла: ")
+        decoded_json = load_data(path_to_json)
+        big_bar = get_biggest_bar(decoded_json)
+        small_bar = get_smallest_bar(decoded_json)
         long, lat = get_coords()
-        closest_bar = get_closest_bar(json_content, long, lat)
-        print("Самый большой бар \"{}\"".format(get_name(big_bar)))
-        print("Самый маленький бар \"{}\"".format(get_name(small_bar)))
-        print("Самый близкий бар \"{}\"".format(get_name(closest_bar)))
+        closest_bar = get_closest_bar(decoded_json, long, lat)
     except OSError as err:
-        print("1{}".format(err))
+        print("{}".format(err))
     except ValueError as err:
         print("{}".format(err))
+    else:
+        print("Самый большой бар - {}".format(get_name(big_bar)))
+        print("Самый маленький бар - {}".format(get_name(small_bar)))
+        print("Самый близкий бар - {}".format(get_name(closest_bar)))
 
 
 if __name__ == "__main__":
     main()
+
