f618f793972b027a4b5d1397225756d7f719528d
AlexE <zed-chi@mail.ru>
2018-08-13 14:25:57 +0300

    fixes
    -edited curring func
    -some naming mistakes
    -try-except blocks

--- bars.py
+++ bars.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from math import sqrt
 
 
@@ -11,52 +12,63 @@ def get_seats(bar):
 
 
 def get_coords():
-    lat = float(input("Введите широту: ").strip())
-    long = float(input("Введите долготу: ").strip())
-    return lat, long
+    try:
+        lat = float(input("Введите широту: ").strip())
+        long = float(input("Введите долготу: ").strip())
+        return lat, long
+    except ValueError:
+        return None, None
 
 
-def get_distance(long, lat, bar=None):
-    if bar is None:
+def get_lambda_for_distance_getter(long, lat):
         return lambda bar: sqrt(
             abs(long-bar["geometry"]["coordinates"][0])**2
             + abs(lat-bar["geometry"]["coordinates"][1])**2)
 
 
 def load_data(path_to_json):
-    with open(path_to_json, "r", encoding="utf-8") as json_file:
-        return json.loads(json_file.read())["features"]
+    try:
+        with open(path_to_json, "r", encoding="utf-8") as json_file:
+            return json.loads(json_file.read())["features"]
+    except ValueError:
+        return None
 
 
-def get_biggest_bar(decoded_json):
-    if decoded_json is not None:
-        biggest_bar = max(decoded_json, key=get_seats)
+def get_biggest_bar(bars_list):
+    if bars_list is not None:
+        biggest_bar = max(bars_list, key=get_seats)
         return biggest_bar
 
 
-def get_smallest_bar(decoded_json):
-    if decoded_json is not None:
-        smallest_bar = min(decoded_json, key=get_seats)
+def get_smallest_bar(bars_list):
+    if bars_list is not None:
+        smallest_bar = min(bars_list, key=get_seats)
         return smallest_bar
 
 
-def get_closest_bar(decoded_json, long, lat):
-    if decoded_json is not None and long is not None and lat is not None:
-        closest = min(decoded_json, key=get_distance(long, lat))
+def get_closest_bar(bars_list, distance_getter):
+    if bars_list is not None:
+        closest = min(bars_list, key=distance_getter)
         return closest
 
 
 def main():
     try:
-        path_to_json = input("Введите путь до json файла: ")
-        decoded_json = load_data(path_to_json)
-        big_bar = get_biggest_bar(decoded_json)
-        small_bar = get_smallest_bar(decoded_json)
+        if len(sys.argv) < 2:
+            exit("Пустой путь")
+        else:
+            path_to_json = sys.argv[1]
+        bars_list = load_data(path_to_json)
+        if bars_list is None:
+            exit("Invalid JSON")
+        big_bar = get_biggest_bar(bars_list)
+        small_bar = get_smallest_bar(bars_list)
         long, lat = get_coords()
-        closest_bar = get_closest_bar(decoded_json, long, lat)
+        if long is None and lat is None:
+            exit("Invalid coordinates")
+        distance_getter = get_lambda_for_distance_getter(long, lat)
+        closest_bar = get_closest_bar(bars_list, distance_getter)
     except OSError as err:
-        print("{}".format(err))
-    except ValueError as err:
         print("{}".format(err))
     else:
         print("Самый большой бар - {}".format(get_name(big_bar)))
@@ -66,4 +78,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
